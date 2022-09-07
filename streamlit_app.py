c1b182201e93cb0358c764058b9aecabf55dea38
soul-reaper69 <61816497+soul-reaper69@users.noreply.github.com>
2022-09-08 04:10:21 +0530

    Update streamlit_app.py
    Stored data into a table using pandas and then displaying it on dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,12 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
+#writes data to the screen
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
+
+# stores the data in a table 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# displays the table data
+streamlit.dataframe(fruityvice_normalized)
