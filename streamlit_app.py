53a848587cc22b7c338682a5ac439e54ef4f88ff
soul-reaper69 <61816497+soul-reaper69@users.noreply.github.com>
2022-09-08 21:28:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ try:
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       streamlit.dataframe(fruityvice_normalized)
-except URL Error as e:
+except URLError as e:
     streamlit.error()  
 
 
