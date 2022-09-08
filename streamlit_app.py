6f4aa904b63d363594f14a9482ee85d205973c00
soul-reaper69 <61816497+soul-reaper69@users.noreply.github.com>
2022-09-08 21:27:36 +0530

    Update streamlit_app.py
    added error imports

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,18 +24,16 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-#writes data to the screen
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
-
-# stores the data in a table 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# displays the table data
-streamlit.dataframe(fruityvice_normalized)
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+    else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
+except URL Error as e:
+    streamlit.error()  
 
 
 #streamlot.stop()
